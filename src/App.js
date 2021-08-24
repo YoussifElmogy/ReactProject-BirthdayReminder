9f95574e1f5bebbcd42bed9f1a8f9d1d12361e35
Youssef <abdelmonemyousef@gmail.com>
2021-08-24 13:16:55 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+
+  const [people,setpeople]=useState(data);
+
+  return <main>
+    <section className="container">
+      <h3>{people.length} Birthdays today</h3>
+      {
+        people.map((person)=>{
+          return <List {...person}></List>
+        })
+      }
+    
+      <button onClick={()=>setpeople([])}>Clear All</button>
+
+    </section>
+  </main>;
+}
+
+export default App;
